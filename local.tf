076fc34d6e54c9ab18d253ce95f152ddef0cba8a
Wesley Emery <we3kb.we@gmail.com>
2021-07-31 09:30:54 -0500

    added nsg rule

--- local.tf
+++ local.tf
@@ -2,8 +2,8 @@ locals {
   name = "${data.azurerm_subscription.current.display_name}-${var.names.location}"
 
   ipaddresses = {
-    argocd = coalesce(var.ingress_ip, data.kubernetes_service.argocd.status.0.load_balancer.0.ingress.0.ip)
-    nginx  = coalesce(var.ingress_ip, data.kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip)
+    argocd =  data.kubernetes_service.argocd.status.0.load_balancer.0.ingress.0.ip
+    nginx  = data.kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip
   }
 
   rule_list = flatten([
